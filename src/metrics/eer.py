ed6a5cc560ffa36fddbc6c400189eb0b70c988f8
“Nastasia <anmafedorova@edu.hse.ru>
2026-08-01 14:25:17 +0300

    2

--- src/metrics/eer.py
+++ src/metrics/eer.py
@@ -6,11 +6,7 @@ from src.metrics.base_metric import BaseMetric
 
 def compute_eer(scores, labels):
 
-    thresholds = np.linspace(
-        0,
-        1,
-        10000
-    )
+    thresholds = np.linspace(scores.min(), scores.max(), 1000)
 
     fars = []
     frrs = []
@@ -19,20 +15,16 @@ def compute_eer(scores, labels):
 
         predict = scores >= threshold
 
-        false_accept = (
-            (predict == 1)
-            &
-            (labels == 0)
-        ).sum()
+        false_accept = np.sum(
+            (predict == 1) & (labels == 0)
+        )
 
-        false_reject = (
-            (predict == 0)
-            &
-            (labels == 1)
-        ).sum()
+        false_reject = np.sum(
+            (predict == 0) & (labels == 1)
+        )
 
-        bona = (labels == 0).sum()
-        spoof = (labels == 1).sum()
+        bona = np.sum(labels == 0)
+        spoof = np.sum(labels == 1)
 
         far = false_accept / max(bona, 1)
         frr = false_reject / max(spoof, 1)
@@ -40,17 +32,12 @@ def compute_eer(scores, labels):
         fars.append(far)
         frrs.append(frr)
 
-
     fars = np.array(fars)
     frrs = np.array(frrs)
 
-    idx = np.argmin(
-        np.abs(fars - frrs)
-    )
+    idx = np.argmin(np.abs(fars - frrs))
 
-    return float(
-        (fars[idx] + frrs[idx]) / 2
-    )
+    return (fars[idx] + frrs[idx]) / 2
 
 
 class EER(BaseMetric):
